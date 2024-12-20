5e06c10eb10212251dd2a9cf6ed8539a7ed8c08e
Greengrinch-17 <greengrinch780@gmail.com>
2024-12-20 20:44:41 +0000

    Add files via upload

---
+++ full_name.py
@@ -0,0 +1,13 @@
+name=input("Enter your full name :")
+if len(name)==0:
+    print("You did'nt enter anything enter your full name:")
+if len(name)<4 :
+    print("You've entered less than 4 characters Please Make sure that you have entered your name and surname ")
+if len(name )>25:
+    print("Please make sure that you have only entered your full name")
+    
+else:
+    
+    print ("Thank you for entering your name")
+    
+          
